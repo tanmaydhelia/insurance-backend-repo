f469ff84d5c52804ee45f8df5d96f062f2178a8e
tanmaydhelia <tanmaydhelia03@gmail.com>
2025-12-28 14:04:51 +0530

    Start: Initializing Services

---
+++ insurace-identity-service/src/test/java/com/identity_service/InsuraceIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuraceIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
