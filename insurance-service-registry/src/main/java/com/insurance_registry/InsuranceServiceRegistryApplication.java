a95038ad8b361044c4e44b99013a95392665d447
tanmaydhelia <tanmaydhelia03@gmail.com>
2025-12-29 13:03:07 +0530

    inti: eureka service registry

---
+++ insurance-service-registry/src/main/java/com/insurance_registry/InsuranceServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.insurance_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ insurance-service-registry/src/test/java/com/insurance_registry/InsuranceServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
