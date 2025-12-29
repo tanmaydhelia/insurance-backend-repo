0bdefb8581c8184746d7dd9a8dfd731abc324592
tanmaydhelia <tanmaydhelia03@gmail.com>
2025-12-29 17:21:21 +0530

    init: Made a spring starter proj for Claims (#5)

---
+++ insurance-claims-service/src/test/java/com/insurance_claims/InsuranceClaimsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance_claims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceClaimsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
